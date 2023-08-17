09d0b62698126e19c0154493b7e5063e10027a6e
003 <anbschool0003@003ui-Macmini.local>
2023-08-17 19:06:11 +0700

    update

--- Building reusseable components/books/src/App.js
+++ Building reusseable components/books/src/App.js
@@ -1,15 +1,18 @@
 import {useState} from 'react';
 import BookCreate from './component/bookCreate';
 function App(){
-
-    const [books, setBooks] = useState([])//To add adition book to the array
-    const createBook = (title)=>{ //event handler
-        console.log('Need to add book with:', title);
-    }
+    const [books, setBooks] = useState([]);
+   
+    const createBook = (title) =>{
+        const updatedBook = [...books, {id:123,title}];
+        setBooks(updatedBook);
+    };
     return (
-        <div>
-            <bookCreate onCreate={createBook}/>
-        </div>
-    );
+    <div>
+        {books.length}
+        <BookCreate onCreate={createBook}/>
+    </div>
+    )
+   
 }
 export default App;
